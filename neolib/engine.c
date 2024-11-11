c8d5557bfa51e9f4c90c530fe76bb868eb1a7a53
Anzle Chavez <anzlemchavez@gmail.com>
2024-11-11 13:52:45 -0500

    Dynamic amount of bodies

--- neolib/engine.c
+++ neolib/engine.c
@@ -1,7 +1,9 @@
 #include <math.h>
 #include <neolog/neolog.h>
+#include <string.h>
 #include "engine.h"
 #include "neolib/constants.h"
+#include "neolib/models.h"
 #include "rigid_body.h"
 
 // NOTE: we determine the window coordinates from -960, 960 and -540, 540.
@@ -9,16 +11,22 @@
 
 static nlog_logger_t log_config = {.LogLevel = DEBUG, "neophys::engine"};
 
-systemstate_t* system_init(float time_step) {
+systemstate_t* system_init(float time_step, _Bool gravity) {
     systemstate_t* state = malloc(sizeof(systemstate_t));
-    state->time_step = time_step;
-    rigidbody_t* body = malloc(sizeof(rigidbody_t));
-    body->s[0] = 0.0f;
-    body->s[1] = 0.0f;
-    body->v[0] = 0.0f;
-    body->v[1] = 0.0f;
-    state->body = body;
 
+    state->time_step = time_step;
+    state->bodies = malloc(sizeof(rigidbody_t) * MAX_BODIES);
+    state->n_bodies = 0;
+    state->forces = malloc(sizeof(force_t) * MAX_FORCES);
+    state->n_forces = 0;
+
+    add_body(state, 20, NULL, NULL);
+
+    if (gravity) {
+        state->forces[0] = GravityForce(state->bodies, state->n_bodies);
+        state->n_forces++;
+    }
+    
     nlog_log(&log_config, DEBUG, "System initialized with timestep %f", time_step);
 
     return state;
@@ -28,12 +36,31 @@ void system_destroy(systemstate_t* state) {
     free(state);
 }
 
-void generate_circle(systemstate_t* state, float circle[CIRCLE_RES * 2]) {
-    circle[0] = (state->body->s[0]) / (state->width / 2.0f);
-    circle[1] = (state->body->s[1]) / (state->height / 2.0f);
-    for (int i = 0; i < CIRCLE_RES-2; i++) {
-	    circle[(i+1)*2 + 0] = (state->size * cosf(i*2*M_PI / (CIRCLE_RES - 3)) + state->body->s[0])/(state->width / 2.0f);
-	    circle[(i+1)*2 + 1] = (state->size * sinf(i*2*M_PI / (CIRCLE_RES - 3)) + state->body->s[1])/(state->height / 2.0f);
+void generate_circle(rigidbody_t* body, systemstate_t* state, float circle[CIRCLE_RES * 2]) {
+    circle[0] = (body->s[0]) / (state->width / 2.0f);
+    circle[1] = (body->s[1]) / (state->height / 2.0f);
+    
+    for (int i = 0; i < CIRCLE_RES - 2; i++) {
+	    circle[(i+1)*2 + 0] = (state->particle_size * cosf(i*2*M_PI / (CIRCLE_RES - 3)) + body->s[0])/(state->width / 2.0f);
+	    circle[(i+1)*2 + 1] = (state->particle_size * sinf(i*2*M_PI / (CIRCLE_RES - 3)) + body->s[1])/(state->height / 2.0f);
     }
-    nlog_log(&log_config, DEBUG, "Scalar used in x: %f", state->width/2.0f);
+    
+    nlog_log(&log_config, DEBUG, "Current circle coordinates: %f, %f", state->bodies[0]->s[0], state->bodies[0]->s[1]);
+}
+
+void step(systemstate_t* state, int n_force, float (*force[n_force]) (float, float)) {
+
+}
+
+void add_body(systemstate_t* state, double mass, double s[2], double v[2]) {
+    rigidbody_t* body = malloc(sizeof(rigidbody_t));
+    
+    if (s == NULL) {
+        memset(body->s, 0, 2);
+    } if (v == NULL) {
+        memset(body->v, 0, 2);
+    }
+
+    state->bodies[(state->n_bodies)++] = body;
+
 }
--- neolib/engine.h
+++ neolib/engine.h
@@ -1,24 +1,32 @@
 #pragma once
 #include "constants.h"
+#include "models.h"
 #include "rigid_body.h" 
 #include <stdlib.h> // IWYU pragma: keep
 
 typedef struct SystemState {
-    float time_step;
     float del_time;
+
     unsigned int VAO;
     unsigned int VBO;
     unsigned int shader_obj;
-    float size;
-    float particle_color[4];
-    rigidbody_t* body;
-
     float width;
     float height;
+
+    // Configuration
+    float time_step;
+    float particle_size;
+    float particle_color[4];
+    
+    rigidbody_t** bodies; // Store as pointers; I predict that we will need to move around bodies a lot.
+    size_t n_bodies;
+    
+    force_t** forces;
+    size_t n_forces;
 } systemstate_t;
 
-systemstate_t* system_init(float time_step);
+systemstate_t* system_init(float time_step, _Bool gravity);
 void system_destroy(systemstate_t* state);
-void add_body(systemstate_t* state);
-void step(systemstate_t* state);
-void generate_circle(systemstate_t* state, float circle[CIRCLE_RES * 2]);
+void add_body(systemstate_t* state, double mass, double s[2], double v[2]);
+void step(systemstate_t* state, int n_force, float (*force[n_force]) (float, float));
+void generate_circle(rigidbody_t* body, systemstate_t* state, float circle[CIRCLE_RES * 2]);
