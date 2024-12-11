ae532f083dd40a5b7e025a5b15d3c62f5acda71c
Anzle Chavez <anzlemchavez@gmail.com>
2024-12-10 19:05:53 -0500

    Created bounds_t object for prototype collision detection

--- neolib/rigid_body.h
+++ neolib/rigid_body.h
@@ -9,7 +9,40 @@ typedef enum RigidShape {
 typedef struct RigidBody {
     shape_e shape;
     float m;
+    union { // Holds the size of the rigid body. Currently only supports circles and squares 
+        float r;
+        struct {float x; float y;};
+    };
     vec2 s;
     vec2 v;
     vec2 f; // This is our force accumulator, that collects all the forces in the x and y direction.
 } rigidbody_t;
+
+typedef struct Boundary {
+    float x;
+    float y;
+    float w;
+    float h;
+} bounds_t;
+
+bounds_t get_bounds(rigidbody_t *body) {
+    bounds_t bounds;
+    switch (body->shape) {
+        case Circle:
+            bounds.x = body->s[0];
+            bounds.y = body->s[1];
+            bounds.w = body->r;
+            bounds.h = body->r;
+            break;
+        case Square:
+            bounds.x = body->s[0];
+            bounds.y = body->s[1];
+            bounds.w = body->x;
+            bounds.h = body->y;
+            break;
+    }
+
+    return bounds;
+}
+
+// TODO: add functions to get vertices of bounds
