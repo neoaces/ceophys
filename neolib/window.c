8e9ee3e27c4b8ff8f68e54b856130faaced1f060
Anzle Chavez <anzlemchavez@gmail.com>
2024-12-21 21:34:06 -0500

    Added test sliders for prototyping

--- neolib/window.c
+++ neolib/window.c
@@ -6,17 +6,16 @@
 
 void draw(config_t* config) {
     BeginDrawing();
+    ClearBackground(WHITE);
 
-    Color background = {.r = 2, .g = 46, .b = 58, .a = 255};
-    ClearBackground(background);
-
-    GuiGroupBox((Rectangle){ 20, 20, 276, 312 }, "STANDARD");
-    GuiTextBox((Rectangle){ 30, 67, 120, 24 }, "TEXT 1", 128, false);
-    GuiTextBox((Rectangle){ 30, 37, 120, 24 }, "TEXT 2", 128, false);
+    GuiGroupBox((Rectangle){ 20, 30, 276, 312 }, "STANDARD");
+    GuiLabel((Rectangle){ 30, 40, 200, 20 }, "TEST OBJECT");
+    GuiSliderBar((Rectangle){ 30, 60, 200, 20 }, "", " X", &config->bodies[0]->x, 0.0f, (config->window_config.width / 10.0f));
+    GuiSliderBar((Rectangle){ 30, 90, 200, 20 }, "", " Y", &config->bodies[0]->y, 0.0f, (config->window_config.height / 10.0f));
 
     for (int i = 0; i < config->objects; i++) {
         body_t* body = config->bodies[i];
-        DrawCircle(body->x * M_TO_PX, body->y * M_TO_PX, body->size * M_TO_PX, body->color);
+        DrawCircle(body->x * M_TO_PX, body->y * M_TO_PX, body->size * M_TO_PX, BLACK);
     }
 
     EndDrawing();
@@ -25,8 +24,8 @@ void draw(config_t* config) {
 void window_init(sconfig_t *config) {
     InitWindow(config->width, config->height, "neophys");
     SetTargetFPS(60);
-    GuiLoadStyle("./static/style.rgs");
-    GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 15);
+    SetWindowState(FLAG_WINDOW_UNDECORATED); // Removes all decoration from window
 }
 
 void window_close() {
