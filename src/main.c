f8f83f45376348acdf4487a4f7d8bfab74314988
Anzle Chavez <anzlemchavez@gmail.com>
2024-12-21 20:37:01 -0500

    Raygui prototyping

--- src/main.c
+++ src/main.c
@@ -1,16 +1,15 @@
-#include <neolib/window.h>
 #include <neolib/neolib.h>
-#include <raylib.h>
-#include <stdio.h>
 
 int main() {
-    sconfig_t config = {.width = 1920, .height = 1080};
+    sconfig_t window_config = {.width = 1600, .height = 1000};
+    config_t config = {.window_config = window_config, 0 , NULL};
     
-    window_init(&config);
+    window_init(&config.window_config);
+    engine_init(&config);
 
     // Main game loop
     while (!WindowShouldClose()) {
-        draw();
+        draw(&config); // Might cause segfault
     }
 
     window_close();
